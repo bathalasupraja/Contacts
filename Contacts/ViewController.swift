7c85cb6c0d605b6ffd0ddb1da3ac08e121950db1
bathalasupraja <bathalasupraja2@gmail.com>
2024-05-24 20:01:36 +0530

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Supraja on 24/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
